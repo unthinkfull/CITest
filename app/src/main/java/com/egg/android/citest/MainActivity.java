2b98ab88e5b86b6b1072614a8ccd07270e4b39c8
unthinkfull <unthinkfull@sina.com>
2016-09-06 18:05:36 +0800

    CI test 1

--- app/src/main/java/com/egg/android/citest/MainActivity.java
+++ app/src/main/java/com/egg/android/citest/MainActivity.java
@@ -2,12 +2,23 @@ package com.egg.android.citest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mTextContent = (TextView) findViewById(R.id.text_main_content);
+        mTextContent.setText("CI is OK");
+
     }
 }
