b4e49e9b1dea1fc4d3a7c89f38d52e5b8e9481cd
unthinkfull <unthinkfull@sina.com>
2016-09-08 10:50:44 +0800

    CI test 5 release to fir.im

--- app/src/main/java/com/egg/android/citest/MainActivity.java
+++ app/src/main/java/com/egg/android/citest/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         mTextContent = (TextView) findViewById(R.id.text_main_content);
         mTextContent.setText("CI is OK");
-        mTextContent.setTextColor(Color.BLUE);
+        mTextContent.setTextColor(Color.GREEN);
 
     }
 }
